a9bfbcad16d4af7a50b355724f18d1a444cd113a
alex-hudenko <ahudenko@gmail.com>
2018-04-14 14:30:14 +0300

    Added delete

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 $(function () {
+
   var ENTER = 13;
-  $('input.new-todo').keypress(function (event) {
-    if (event.keyCode === ENTER) {
-      $('ul.todo-list').append('<li>\
-      <div class="view">\
-        <input class="toggle" type="checkbox">\
-        <label>Test title</label>\
-        <button class="destroy"></button>\
-      </div>\
-      <input class="edit" value="Test title">\
-    </li>');
+
+  $('input.new-todo').keypress(function (e) {
+    if (e.keyCode === ENTER) {
+      var input = e.target;
+      var value = input.value.trim();
+      if (value.length > 0) {
+        $('ul.todo-list').append('<li>\
+          <div class="view">\
+            <input class="toggle" type="checkbox">\
+            <label>'+ value + '</label>\
+            <button class="destroy"></button>\
+          </div>\
+          <input class="edit" value="'+ value + '">\
+        </li>');
+      }
+      input.value = '';
     }
   });
+
+  $('body').on('click', 'button.destroy', function(e){
+    $(e.target).parents('li').remove();
+  });
+
+
+
 });
