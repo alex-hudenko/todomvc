efcc220997fc481a4c083bdd1fe7b9ff0d6c5bb4
alex-hudenko <ahudenko@gmail.com>
2018-04-14 13:58:33 +0300

    Add sample todo

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 $(function () {
-  console.log('index.js');
+  var ENTER = 13;
+  $('input.new-todo').keypress(function (event) {
+    if (event.keyCode === ENTER) {
+      $('ul.todo-list').append('<li>\
+      <div class="view">\
+        <input class="toggle" type="checkbox">\
+        <label>Test title</label>\
+        <button class="destroy"></button>\
+      </div>\
+      <input class="edit" value="Test title">\
+    </li>');
+    }
+  });
 });
