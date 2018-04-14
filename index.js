9242f7701a677996406aaf5ce74c09a238c70f49
alex-hudenko <ahudenko@gmail.com>
2018-04-14 15:08:15 +0300

    Add checkboxes; Add filters

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $(function () {
 
   var ENTER = 13;
 
+  // init input
   $('input.new-todo').keypress(function (e) {
     if (e.keyCode === ENTER) {
       var input = e.target;
@@ -20,10 +21,39 @@ $(function () {
     }
   });
 
+  // init checkbox
+  $('body').on('change', 'input.toggle', function(e){
+    var checkbox =  e.target;
+    if (checkbox.checked) {
+      $(e.target).parents('li').addClass('completed');
+    } else {
+      $(e.target).parents('li').removeClass('completed');
+    }
+  });
+
+  // init delete button
   $('body').on('click', 'button.destroy', function(e){
     $(e.target).parents('li').remove();
   });
 
+  // init filters
+  $('ul.filters a').click(function(e){
+    var a = e.target;
+    $('ul.filters a').removeClass('selected');
+    $(a).addClass('selected');
 
+    if (a.hash === '#/') {
+      $('ul.todo-list li').show();
+    }
+    if (a.hash === '#/active') {
+      $('ul.todo-list li').show();
+      $('ul.todo-list li.completed').hide();
+    }
+    if (a.hash === '#/completed') {
+      $('ul.todo-list li').hide();
+      $('ul.todo-list li.completed').show();
+    }
+
+  })
 
 });
